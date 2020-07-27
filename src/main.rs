5202d5d015e306bb19fb190f71924bb67be09d88
Adityo Pratomo <adityo@tetrate.io>
2020-07-27 14:57:24 +0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,6 @@ use std::io::prelude::*;
 
 mod governor_figma;
 
-// mod governor_figma {
-//     fn get_page() -> Result<(), Box<dyn std::error::Error>> {
-//     let figma_token = "47459-dc550ef0-84e2-4bdc-8c02-65d223b2f6fb";
-//     let _figma_node_id = "your-targeted-node-id";
-
-//     let res = reqwest::Client::new()
-//         .get("https://api.figma.com/v1/files/iRGl920bR0gkzDcLazgf0D/")
-//         .header(
-//             "X-FIGMA-TOKEN",
-//             figma_token
-//         )
-//         .send()
-//         .await?;
-
-//     println!("Status: {}", res.status());
-
-//     let body = res.text().await?;
-
-//     println!("Preparing the component description file");
-//     let mut file = File::create("component.json")?;
-//     file.write_all(body.as_bytes())?;    
-//     println!("component.json has been created");
-
-//     Ok(())
-//     }
-// }
-
 fn main()  {
     governor_figma::get_page().unwrap();
     create_component_dir().unwrap();
