dab89cc54f6e6da98baf3f450657e4fd95a961ce
Adityo Pratomo <adityo@tetrate.io>
2020-07-27 12:23:34 +0700

    barebone method to generate React component

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ extern crate tokio;
 use serde::Deserialize;
 use serde_json::Value;
 
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -26,8 +28,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let body = res.text().await?;
 
+    println!("Preparing the component description file");
     let mut file = File::create("component.json")?;
     file.write_all(body.as_bytes())?;    
+    println!("component.json has been created");
 
+    create_component_dir().unwrap();
+    create_react_component().unwrap();
+
+    Ok(())
+}
+
+fn create_react_component() -> std::io::Result<()>{
+
+    let mut header = String::new();
+    header.push_str("import React from 'react' \n");
+    header.push_str("import './styles.css' \n");
+
+    let html_element_name = "button";
+    let html_element = uppercase_first_letter(html_element_name);
+
+    let component_declaration = format!("export default function {}() {{ \n \t return ( \n", html_element);
+
+    let mut body = String::new();
+    body.push_str(&component_declaration);
+    body.push_str("\t ); \n }");
+
+    let mut component = String::new();
+    component.push_str(&header);
+    component.push_str(&body);
+
+    let mut file = File::create("component/button.jsx")?;
+    file.write_all(component.as_bytes())?;    
+
+    Ok(())
+}
+
+fn uppercase_first_letter(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().chain(c).collect()
+    }
+}
+
+fn create_component_dir() -> std::io::Result<()> {
+    fs::create_dir_all("component")?;
     Ok(())
 }
